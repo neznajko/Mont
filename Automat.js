648e507d51b0958f9b29a15989afac349d6ae7bd
D.Panayotov <we4er@abv.com>
2023-05-19 16:16:11 +0300

    🐟

--- Automat.js
+++ Automat.js
@@ -1,79 +1,7 @@
 ////////////////////////////////////////////////////////////////
-import { monospace } from "./monospace.js";
-import { Charmat } from "./Charmat.js";
-////////////////////////////////////////////////////////////////
-const X = 0;
-const Y = 1;
-////////////////////////////////////////////////////////////////
-class Point extends Array {
-    clone() {
-        return this.slice();
-    }
-    add( point ){
-        this.map(( value, j ) => {
-            this[ j ] += point[ j ];
-        });
-        return this;
-    }
-    sub( point ){
-        this.map(( value, j ) => {
-            this[ j ] -= point[ j ];
-        });
-        return this;
-    }
-    mul( n ){
-        this.map(( value, j ) => {
-            this[ j ] *= n; 
-        });
-        return this;
-    }
-    div( n ){
-        return this.mul( 1/ n );
-    }
-}
-////////////////////////////////////////////////////////////////
-class Polygon extends Array {
-    clone() {
-        return this.map( point => {
-            return point.clone();
-        });
-    }
-    add( polygon ){
-        this.map(( point, j ) => {
-            point.add( polygon[ j ]);
-        });
-        return this;
-    }
-    sub( polygon ){
-        this.map(( point, j ) => {
-            point.sub( polygon[ j ]);
-        });
-        return this;
-    }
-    mul( n ){
-        this.map( point => {
-            point.mul( n );
-        });
-        return this;
-    }
-    div( n ){
-        return this.mul( 1/ n );
-    }
-    translate( offset ){
-        this.map( point => {
-            point.add( offset );
-        });
-        return this;
-    }
-    createPath( ctx ){
-        ctx.beginPath();
-        ctx.moveTo( this[ 0 ][ X ], this[ 0 ][ Y ]);
-        for( let j = 1, n = this.length; j < n; j++ ){
-            ctx.lineTo( this[ j ][ X ], this[ j ][ Y ]);
-        }
-        ctx.closePath();
-    }
-}
+import { monospace      } from "./monospace.js";
+import { Charmat        } from "./Charmat.js";
+import { Point, Polygon } from "./Polygon.js";
 ////////////////////////////////////////////////////////////////
 class Alphabet {
     static BaseFontsize = 100; // px
@@ -102,82 +30,8 @@ class Alphabet {
     }
 }
 ////////////////////////////////////////////////////////////////
-// #abc => [ 10, 11, 12 ]
-function hexStringToArray( hexString ){
-    return [...hexString.slice( 1 )].map( v => {
-        return +( "0x" + v );
-    });
-}
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-class Gradient {
-    static linearFactory( x, y, qq ){
-    // y = Ax + B
-    // y1 = Ax1 + B
-    // y2 = Ax2 + B
-    // A = (y2 - y1)/(x2 - x1)
-    // B = (x2y1 - x1y2)/(x2 - x1)
-        const dX = x[ 1 ] - x[ 0 ];
-        const A = (y[ 1 ] - y[ 0 ])/ dX;
-        const B = (x[ 1 ] * y[ 0 ] - x[ 0 ] * y[ 1 ])/ dX;
-        const m = Math.min( y[ 0 ], y[ 1 ]);
-        return u => {
-            let v = A * u + B;
-            if( u > x[ 0 ] && u < x[ 1 ]){
-                v = m + qq * ( v - m ); // <1>
-            }
-            return Math.round( v );
-        }
-    }
-    static linearStuff({
-        nfFrames,
-        colorFrom,
-        colorTo,
-        qq,
-    }){
-        // <
-        const color = Array.from({ length: nfFrames });
-        const y = Gradient.linearFactory([ 0, nfFrames - 1 ], 
-                                         [ colorFrom, colorTo ],
-                                         qq );
-        for( let frame = 0; frame < nfFrames; frame++ ){
-            color[ frame ] = y( frame );
-        }
-        return color;
-    }
-    constructor({
-        hexString,
-        nfFrames,
-        qq,
-    }){
-        const FROM = 0,
-              TO = 1;
-        const hexArrayFrom = hexStringToArray( hexString[ FROM ]);
-        const hexArrayTo = hexStringToArray( hexString[ TO ]);
-        const R = 0, 
-              G = 1, 
-              B = 2;
-        const rgb = [ R, G, B ].map( j => {
-            return this.constructor.linearStuff({
-                nfFrames: nfFrames,
-                colorFrom: hexArrayFrom[ j ],
-                colorTo: hexArrayTo[ j ],
-                qq: qq,
-            }).map( v => {
-                return v.toString( 16 );
-            });
-        });
-        const r = rgb[ R ];
-        const g = rgb[ G ];
-        const b = rgb[ B ];
-        this.color = Array.from({ length: nfFrames }).map(( v, j ) => {
-            return `#${r[ j ]}${g[ j ]}${b[ j ]}`;
-        });
-        this.nfFrames = nfFrames;
-    }
-}
 ////////////////////////////////////////////////////////////////
 function Split( str, nfChar ){
     const n = str.length;
@@ -201,10 +55,6 @@ function Split( str, nfChar ){
 ///////////////////////////////////////////////////////////////
 function Align( lst, nfChar ){
     return lst.map( s => s + ' '.repeat( nfChar - s.length ));
-}
-///////////////////////////////////////////////////////////////
-function getRandomInt( min, max ){ // [ min, max ]
-    return Math.floor( Math.random() *( max - min + 1 )) + min;
 }
 ///////////////////////////////////////////////////////////////
 class Automat {
@@ -285,7 +135,7 @@ class Automat {
         }
     }
 }
-export { X, Y, Point, Automat };
+export { Automat };
 ////////////////////////////////////////////////////////////////
 // DOTO: - ` ~ @ # $  
 //         ^ & { } [  
