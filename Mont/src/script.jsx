97c2b253969260aa2e40009645288ecd16b6ab54
we4er <neznajko@yahoo.com>
2024-01-25 16:41:53 +0200

    LOGBOOK

--- Mont/src/script.jsx
+++ Mont/src/script.jsx
@@ -4,30 +4,51 @@
 ////////////////////////////////////////////////////////////////
 import './style.css'
 import { Cruiser } from './Cruiser'
+import { Publisher } from './Publisher'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
+const txt = `In a hole in the ground there lived a hobbit. Not a 
+nasty, dirty, wet hole, filled with the ends of worms and an 
+oozy smell, nor yet a dry, bare, sandy hole with nothing in it 
+to sit down on or to eat: it was a hobbit-hole, and that means
+comfort.
+It had a perfectly round door like a porthole, painted green,
+with a shiny yellow brass knob in the exact middle. The door
+opened on to a tubeshaped hall like a tunnel: a very comfortable
+tunnel without smoke, with panelled walls, and floors tiled and
+carpeted, provided with polished chairs, and lots and lots of
+pegs for hats and coats - the hobbit was fond of visitors. The
+tunnel wound on and on, going fairly but not quite straight into
+the side of the hill - The Hill, as all the people for many
+miles round called it - and many little round doors opened out
+of it, first on one side and then on another. No going upstairs
+for the hobbit: bedrooms, bathrooms, cellars, pantries (lots of
+these), wardrobes (he had whole rooms devoted to clothes),
+kitchens, dining-rooms, all were on the same floor, and indeed
+on the same passage. The best rooms were all on the left-hand
+side (going in), for these were the only ones to have windows,
+deep-set round windows looking over his garden and meadows
+beyond, sloping down to the river.`
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-const LOGBOOK = [
-  "Vast expanses of blue waters stretch endlessly",
-  "Waves crash, painting a rhythmic dance",
-  "Mysteries lie deep, waiting to unveil",
-  "Sunsets reflect, shimmering golden hues",
-  "Creatures dwell, adapting to the depths",
-  "Tides ebb and flow, shaping coastlines",
-  "Storms brew, showcasing nature's raw power",
-  "Oceans connect, bridging distant lands"
-];
+////////////////////////////////////////////////////////////////
+const cruiserid = 'cruiser';
+////////////////////////////////////////////////////////////////
+const LOGBOOK = new Publisher( txt, cruiserid ).getbook();
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
-ReactDOM.createRoot( document.getElementById( 'cruiser' ))
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+ReactDOM.createRoot( document.getElementById( cruiserid ))
         .render( <React.StrictMode> 
-                   <Cruiser siz={ 7 }
-                            logbook={ LOGBOOK } />
+                   <Cruiser siz={ LOGBOOK[ 0 ].length }
+                            logbook={ LOGBOOK[ 0 ]} />
                  </React.StrictMode> );
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
